236476623e7af660a0b81249e9829afe91780dcb
yanghuadong <314726028@qq.com>
2018-12-06 11:16:32 +0800

    提交java8测试类

---
+++ src/main/java/yhd/java/java8/Java8Demo.java
@@ -0,0 +1,11 @@
+package yhd.java.java8;
+
+/**
+ * @author: yang
+ * @date: 2018/12/6
+ */
+public class Java8Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
